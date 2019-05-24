ccafdb0bf9e2bab5dbd529052ff785762820243e
Michał Stachel <michal.stachel@gmail.com>
2019-05-24 22:55:47 +0200

    Updates

---
+++ precache-manifest.d3d89e2bf370fdc570baa41ec098c757.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcd435ff9a6d76efd61a",
+    "url": "/Photography-Page/static/css/main.8a5d5ad6.chunk.css"
+  },
+  {
+    "revision": "bcd435ff9a6d76efd61a",
+    "url": "/Photography-Page/static/js/main.b682e51d.chunk.js"
+  },
+  {
+    "revision": "c59b5ad88aa2135d807b",
+    "url": "/Photography-Page/static/js/runtime~main.0e3c98a3.js"
+  },
+  {
+    "revision": "990092f67e3c737d70d2",
+    "url": "/Photography-Page/static/css/2.921fc2f8.chunk.css"
+  },
+  {
+    "revision": "990092f67e3c737d70d2",
+    "url": "/Photography-Page/static/js/2.ea31facd.chunk.js"
+  },
+  {
+    "revision": "684d9e569cc52b068d7d6aa3ce443cf9",
+    "url": "/Photography-Page/static/media/Bigger_BabyW.684d9e56.jpg"
+  },
+  {
+    "revision": "5145d8d0e440ebaf8dcc6cdb5497fb8a",
+    "url": "/Photography-Page/static/media/BabyW.5145d8d0.jpg"
+  },
+  {
+    "revision": "4d5aa0e2d43ab0547614895d8a3a9ed8",
+    "url": "/Photography-Page/static/media/Smaller_BabyW.4d5aa0e2.jpg"
+  },
+  {
+    "revision": "ab257b37b024b6144bc892ad47da8001",
+    "url": "/Photography-Page/static/media/Pregnant_WomanW.ab257b37.jpg"
+  },
+  {
+    "revision": "72f02742077412022183ad00ffb96400",
+    "url": "/Photography-Page/static/media/Logo.72f02742.png"
+  },
+  {
+    "revision": "639603d3463109fd4af9815154afcc16",
+    "url": "/Photography-Page/static/media/arrowLeft.639603d3.png"
+  },
+  {
+    "revision": "d4bc6ff48b983c9e13eee6f31e8a0452",
+    "url": "/Photography-Page/static/media/arrowRight.d4bc6ff4.png"
+  },
+  {
+    "revision": "69ca37b64f74805b622d26f3e62586d0",
+    "url": "/Photography-Page/index.html"
+  }
+];
