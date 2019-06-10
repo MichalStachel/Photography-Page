d736824d038ac7a8f34b6d97b60847fd6bb1a8c5
Michał Stachel <michal.stachel@gmail.com>
2019-06-10 22:56:40 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Photography-Page/./precache-manifest.6637b711fddd670038e8f6e04644fa78.js"
+  "/Photography-Page/./precache-manifest.544bb6d7ad83c4f9dbd5c7bd41703a50.js"
 );
 
 workbox.clientsClaim();
