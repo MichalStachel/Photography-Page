8d579c95ef3b3fca2d0ff7b9566c137c3a0de80c
Michał Stachel <michal.stachel@gmail.com>
2019-06-10 23:35:06 +0200

    add photos

--- src/Components/Layouts/menu.jsx
+++ src/Components/Layouts/menu.jsx
@@ -15,7 +15,7 @@ const Menu = (props) => {
       <li onClick={handleUp}>Strona Główna</li>
       <li onClick={props.handleChangeToFalse}><Link activeClass="active" to="scrollToKids" spy={true} smooth={true} duration={500} >Sesje Dziecięce</Link></li>
       <li onClick={props.handleChangeToFalse}><Link activeClass="active" to="scrollToBaby" spy={true} smooth={true} duration={500} >Sesje Noworodków</Link></li>
-      <li onClick={props.handleChangeToFalse}><Link activeClass="active" to="scrollToWedding" spy={true} smooth={true} duration={500} >Sesje ślubne</Link></li>
+      <li onClick={props.handleChangeToFalse}><Link activeClass="active" to="scrollToWedding" spy={true} smooth={true} duration={500} >Sesje ciążowe</Link></li>
       <li onClick={props.handleChangeToFalse}><Link activeClass="active" to="scrollToAbout" spy={true} smooth={true} duration={500} >O mnie</Link></li>
       <li onClick={props.handleChangeToFalse}><Link activeClass="active" to="scrollToContact" spy={true} smooth={true} duration={500} >Kontakt</Link></li>
     </ul>
--- src/Components/Pages/KidsPhotos.jsx
+++ src/Components/Pages/KidsPhotos.jsx
@@ -1,46 +1,44 @@
 import React, { Component } from 'react';
 import '../Styles/KidsPhotos.css';
-import Bigger_Baby from '../images/Bigger_BabyW.jpg';
-import Baby from '../images/BabyW.jpg';
-import Smaller from '../images/Smaller_BabyW.jpg';
-import Woman from '../images/Pregnant_WomanW.jpg';
 import Item from '../Pages/Item.jsx';
 import ItemBaby from '../Pages/ItemBaby.jsx';
 import ItemWedding from '../Pages/ItemWedding.jsx';
 import ScrollAnimation from 'react-animate-on-scroll';
 import { Element } from 'react-scroll';
+// Photos Kids
+import a from '../images/kids/a.jpg';
+import b from '../images/kids/b.jpg';
+import c from '../images/kids/c.jpg';
+import d from '../images/kids/d.jpg';
+import e from '../images/kids/e.jpg';
+import f from '../images/kids/f.jpg';
+import g from '../images/kids/g.jpg';
+import h from '../images/kids/h.jpg';
+// Photos Baby
+import i from '../images/Baby/i.jpg';
+import j from '../images/Baby/j.jpg';
+import k from '../images/Baby/k.jpg';
+import l from '../images/Baby/l.jpg';
+import m from '../images/Baby/m.jpg';
+import n from '../images/Baby/n.jpg';
+import o from '../images/Baby/o.jpg';
+import p from '../images/Baby/p.jpg';
+// Photo Pregnant
+import q from '../images/Pregnant/q.jpg';
+import r from '../images/Pregnant/r.jpg';
+import s from '../images/Pregnant/s.jpg';
+import t from '../images/Pregnant/t.jpg';
+import u from '../images/Pregnant/u.jpg';
+import v from '../images/Pregnant/v.jpg';
+import w from '../images/Pregnant/w.jpg';
+import x from '../images/Pregnant/x.jpg';
+
+
 
 
 class KidsPhoto extends Component {
 
   state = {
-    kids: [
-      { name: Bigger_Baby, id: 0 },
-      { name: Baby, id: 1 },
-      { name: Smaller, id: 2 },
-      { name: Woman, id: 3 },
-      { name: Bigger_Baby, id: 4 },
-      { name: Baby, id: 5 },
-      { name: Smaller, id: 6 },
-      { name: Woman, id: 7 }],
-    baby: [
-      { name: Smaller, id: 0 },
-      { name: Bigger_Baby, id: 1 },
-      { name: Woman, id: 2 },
-      { name: Bigger_Baby, id: 3 },
-      { name: Smaller, id: 4 },
-      { name: Baby, id: 5 },
-      { name: Baby, id: 6 },
-      { name: Woman, id: 7 }],
-    wedding: [
-      { name: Bigger_Baby, id: 0 },
-      { name: Baby, id: 1 },
-      { name: Smaller, id: 2 },
-      { name: Woman, id: 3, key: 3 },
-      { name: Bigger_Baby, id: 4 },
-      { name: Baby, id: 5 },
-      { name: Smaller, id: 6 },
-      { name: Woman, id: 7 }],
     showAll: false,
     showAllBaby: false,
     showAllWedding: false,
@@ -102,14 +100,41 @@ class KidsPhoto extends Component {
 
 
   render() {
-    const { showAll, showAllBaby, showAllWedding, showPhoto, kids, index, showPhotoBaby, baby, indexBaby, wedding, indexWedding, showPhotoWedding } = this.state
+    const kids = [
+      { name: a, id: 0 },
+      { name: b, id: 1 },
+      { name: c, id: 2 },
+      { name: d, id: 3 },
+      { name: e, id: 4 },
+      { name: f, id: 5 },
+      { name: g, id: 6 },
+      { name: h, id: 7 }];
+    const baby = [
+      { name: i, id: 0 },
+      { name: j, id: 1 },
+      { name: k, id: 2 },
+      { name: l, id: 3 },
+      { name: m, id: 4 },
+      { name: n, id: 5 },
+      { name: o, id: 6 },
+      { name: p, id: 7 }];
+    const pregnant = [
+      { name: q, id: 0 },
+      { name: r, id: 1 },
+      { name: s, id: 2 },
+      { name: t, id: 3 },
+      { name: u, id: 4 },
+      { name: v, id: 5 },
+      { name: w, id: 6 },
+      { name: x, id: 7 }];
+    const { showAll, showAllBaby, showAllWedding, showPhoto, index, showPhotoBaby, indexBaby, indexWedding, showPhotoWedding } = this.state
     const object = kids.map((photo) =>
       <Item key={photo.id} name={photo.name} id={photo.id} show={this.show} />
     );
     const objectBaby = baby.map((photo) =>
       <ItemBaby key={photo.id} name={photo.name} id={photo.id} showBaby={this.showBaby} />
     );
-    const objectWedding = wedding.map((photo) =>
+    const objectWedding = pregnant.map((photo) =>
       <ItemWedding key={photo.id} name={photo.name} id={photo.id} showWedding={this.showWedding} />
     );
 
@@ -145,10 +170,10 @@ class KidsPhoto extends Component {
         </ScrollAnimation>
         {/* Third Element */}
         {showPhotoWedding ?
-          <><Element name='scrollToBigPictureWedding'><div className='closeBtn' onClick={this.showOff}><div className='btnLine' /><div className='btnLineSec' /></div></Element> <img src={wedding[indexWedding].name} alt='' className='show' /></>
-          : <> <Element name='scrollToBigPictureWedding'><div className="closeBtn" onClick={this.showOff}><div className={showPhotoWedding ? 'btnLine' : 'btnLineHide'} /><div className={showPhotoWedding ? 'btnLineSec' : 'btnLineSecHide'} /></div></Element><img src={wedding[indexWedding].name} alt='' className='erase' /></>}
+          <><Element name='scrollToBigPictureWedding'><div className='closeBtn' onClick={this.showOff}><div className='btnLine' /><div className='btnLineSec' /></div></Element> <img src={pregnant[indexWedding].name} alt='' className='show' /></>
+          : <> <Element name='scrollToBigPictureWedding'><div className="closeBtn" onClick={this.showOff}><div className={showPhotoWedding ? 'btnLine' : 'btnLineHide'} /><div className={showPhotoWedding ? 'btnLineSec' : 'btnLineSecHide'} /></div></Element><img src={pregnant[indexWedding].name} alt='' className='erase' /></>}
 
-        <span className='wide kidText'><Element name='scrollToWedding'>Sesje ślubne</Element></span>
+        <span className='wide kidText'><Element name='scrollToWedding'>Sesje ciążowe</Element></span>
         <div className='kids'>
           {showAllWedding ? objectWedding : objectWedding.slice(0, 4)}
         </div>
